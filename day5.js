4f364c84aefd83e1df29928f914429020c6ceeb9
nihaojuhua <heyjuhua@gmail.com>
2020-12-13 17:47:36 -0800

    update day5

--- day5.js
+++
@@ -1,9 +0,0 @@
-function reverseAString(str) {
-  //  write code here.
-  let strArr = [];
-  let length = str.length - 1;
-  for (let i = length; i >= 0; i--) {
-    strArr.push(str[i]);
-  }
-  return strArr.join("");
-}
